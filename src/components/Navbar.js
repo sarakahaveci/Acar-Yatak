dcb1ae5a59464aeacb860ff3e52657f4fc5e83e4
Yamoonz <yamooonz@gmail.com>
2021-02-28 20:38:18 +0300

    --- Auto-Git Commit ---

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,37 @@
-import { Navbar, Nav,form } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import './Navbar.css'
+import { Navbar, Nav, Form, Button } from "react-bootstrap";
 
-export function NavbarComponent() {
+function NavBarComponent() {
   return (
-    <> 
-   
-<Navbar  class="p-3 mb-2 bg-light text-white" >
-    <Navbar.Brand href="#home">
-     <img className="logo" 
-            src="http://www.acaryatak.com/content/assets/default/img/logo.png"
-            width="20%"
-            height="10%"  
-            alt="Acar Yatak logo" />
-            <Navbar class="p-3 bg-light text-dark">
-            <form class="form-inline mx-auto">
-        
-<input class="form-control mx-auto" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-      <Nav className="navbar-nav ml-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#products">Products</Nav.Link>
-      <Nav.Link href="#aboutus">About Us</Nav.Link>
-      <Nav.Link href="#contactus">Contact Us</Nav.Link>
-    </Nav>
+    <Navbar class="p-3 mb-2 bg-light text-white" bg="light" expand="lg">
+      <Navbar.Brand href="#home">
+        <img
+          className="logo"
+          src="http://www.acaryatak.com/content/assets/default/img/logo.png"
+          width="100px"
+          height="10%"
+          alt="Acar Yatak logo"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Form inline>
+            <Form.Control
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#products">Products</Nav.Link>
+          <Nav.Link href="#aboutus">About Us</Nav.Link>
+          <Nav.Link href="#contactus">Contact Us</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
-    </Navbar.Brand>
-          </Navbar>
-      
-    </>
   );
 }
-export default NavbarComponent;
+
+export default NavBarComponent;
