1076d107e8709f98b5ffeb04844b5eabed594f9a
Yamoonz <yamooonz@gmail.com>
2021-03-03 11:58:24 +0300

    fixed footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Cards from './components/Cards';
 import Footer from './components/Footer';
 import NavbarComp from './components/Navbar';
-import {Router, Route} from "react-router-dom";
+
 function App() {
   return (
     <>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import { Link,BrowserRouter } from 'react-router-dom';
 import './Footer.css';
 
 
@@ -45,54 +45,9 @@ function Footer() {
     <p class="name"> Fatih Acar &copy; 2021</p>
   </div>
 </footer>
-        <BrowserRouter>
-        <div className='footer-container'>
-            <div class='footer-links'>
-                <div className='footer-link-wrapper'>
-                <div className='footer-link-wrapper'>
-                <div class='footer-link-items'>
-                    <h2> Products</h2>
-                    <Link to='/'>Beds</Link>
-                    <Link to='/'>Beds Basments</Link>
-                    <Link to='/'>Beds Bunks</Link>
-                </div>
-            </div>  
-                <div class='footer-link-items'>
-                        <h2> Address</h2>
-                        <Link to='/'> Bayrampaşa/İstanbul 34035</Link>
-                       
-                    </div>
-                    <div class='footer-link-items'>
-                    <h2>Contact Us</h2>
-                    <Link to='/'> +90 (537)</Link>
-                        <Link to='/'> +90 (212) 579 80 02</Link>
-                        <Link to='/'> +90 (212) 674 04 73</Link>
-                        <Link to='/'>info@yurosunger.com</Link>
-                    </div>
-                </div>
-                <div class='footer-link-items'>
-                <h2>Social Media</h2>
-                <Link to='/https://www.instagram.com/acaryatak/'>Instagram</Link>
-                <Link to='/https://www.facebook.com/Acaryatak'>Facebook</Link>
-                <Link to='/https://www.youtube.com/channel/UCPT7nYipeN62q5lOHhd3MuA'>Youtube</Link>
-
-            </div>
-            </div>
-            <section class='social-media'>
-                <div class='social-media-wrap'>
-                    <div class='footer-logo'>
-                        <Link to='/' className='social-logo'>
-                          f.acar
-                            <i class='fab fa-typo3' />
-                        </Link>
-                    </div>
-                    <small class='website-rights'>Yaman © 2021</small>
+           <small class='website-rights'>Yaman © 2021</small>
                     <div class='social-icons'>
                     </div>
-                </div>
-            </section>
-        </div>
-        </BrowserRouter>
         </>
     );
 }
