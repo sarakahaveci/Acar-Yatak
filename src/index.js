d46eb21e34579492fdc7f9a67a01b98078375ce3
Yamoonz <yamooonz@gmail.com>
2021-03-03 13:59:34 +0300

    ab

--- src/index.js
+++ src/index.js
@@ -6,16 +6,14 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../../node_modules/bootstrap/dist/css/bootstrap.css"
 import './i18n';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-  </BrowserRouter>
-);
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
