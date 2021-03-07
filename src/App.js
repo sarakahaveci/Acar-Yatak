53507924f1477066ac3c2dde5ef36020fe1d9781
Yamoonz <yamooonz@gmail.com>
2021-03-07 22:17:59 +0300

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -8,24 +8,22 @@ import Home from "./components/home/Home";
 import SalesOutlet from "./components/salesoutlet/Salesoutlet";
 import AboutUs from "./components/aboutus/Aboutus";
 import ContactUs from "./components/contactus/Contactus";
-import { Switch, Route, Router } from "react-router-dom/Route";
+import { Switch, Route } from "react-router-dom/Route";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <NavbarComp />
-        <main>
-          <Switch>
-            <Route path="/" exacts component={Home} />
-            <Route path="/products" component={Products} />
-            <Route path="/salesoutlet" component={SalesOutlet} />
-            <Route path="/aboutus" component={AboutUs} />
-            <Route path="/contacts" component={ContactUs} />
-          </Switch>
-        </main>
-        <Footer />
-      </Router>
+      <NavbarComp />
+      <main>
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/products" component={Products} />
+          <Route path="/salesoutlet" component={SalesOutlet} />
+          <Route path="/aboutus" component={AboutUs} />
+          <Route path="/contacts" component={ContactUs} />
+        </Switch>
+      </main>
+      <Footer />
     </div>
   );
 }
