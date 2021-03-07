711da9324c73f4a2861fe122054d0f3a33371878
Yamoonz <yamooonz@gmail.com>
2021-03-07 21:47:47 +0300

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from "./components/home/Home";
 import SalesOutlet from "./components/salesoutlet/Salesoutlet";
 import AboutUs from "./components/aboutus/Aboutus";
 import ContactUs from "./components/contactus/Contactus";
-import { Switch, Route, Router } from "react-router-dom";
+import { Switch, Route, Router } from "react-router-dom/Route";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         <NavbarComp />
         <main>
           <Switch>
-            <Route path="/" exact component={Home} />
+            <Route path="/" exacts component={Home} />
             <Route path="/products" component={Products} />
             <Route path="/salesoutlet" component={SalesOutlet} />
             <Route path="/aboutus" component={AboutUs} />
