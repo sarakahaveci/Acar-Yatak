491537a0d9bdb349e2b89ec07db7c83df0bcef40
Yamoonz <yamooonz@gmail.com>
2021-03-07 21:17:41 +0300

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-
-import './App.css';
-import Footer from './components/Footer';
-import NavbarComp from './components/Navbar';
-import Products from './components/Products';
-import Home from './components/home/Home';
-import SalesOutlet from './components/salesoutlet/Salesoutlet';
-import AboutUs from './components/aboutus/Aboutus';
-import ContactUs from './components/contactus/Contactus';
-
-import {
-  Switch,
-  Route,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import NavbarComp from "./components/Navbar";
+import Products from "./components/Products";
+import Home from "./components/home/Home";
+import SalesOutlet from "./components/salesoutlet/Salesoutlet";
+import AboutUs from "./components/aboutus/Aboutus";
+import ContactUs from "./components/contactus/Contactus";
+import { Switch, Route, Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    
-    <NavbarComp/>
-       <main>
-           <Switch>
-           <Route path="/home" component={Home} />
-           <Route path="/products" component={Products} />
-           <Route path="/salesoutlet" component={SalesOutlet} />
-           <Route path="/aboutus" component={AboutUs} />
-           <Route path="/contacts" component={ContactUs} />
+      <Router>
+        <NavbarComp />
+        <main>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/products" component={Products} />
+            <Route path="/salesoutlet" component={SalesOutlet} />
+            <Route path="/aboutus" component={AboutUs} />
+            <Route path="/contacts" component={ContactUs} />
           </Switch>
-         </main>
-     <Footer/>
+        </main>
+        <Footer />
+      </Router>
     </div>
   );
 }
