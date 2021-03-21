13e31081cc29362331c328e3849e748a7994ab9d
Yamoonz <yamooonz@gmail.com>
2021-03-21 15:59:53 +0300

    --- Auto-Git Commit ---

--- src/i18n.js
+++ src/i18n.js
@@ -1,29 +1,28 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import translationAR from "./components/locales/ar/translation.json";
+import translationTR from "./components/locales/tr/translation.json";
 
-import Backend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
-// don't want to use this?
-// have a look at the Quick start guide
-// for passing in lng and translations on init
+// the translations
+const resources = {
+  ar: {
+    translation: translationAR,
+  },
+  tr: {
+    translation: translationTR,
+  },
+};
 
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
+  .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    fallbackLng: "en",
-    debug: true,
+    resources,
+    lng: "en",
+
+    keySeparator: false, // we do not use keys in form messages.welcome
 
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false, // react already safes from xss.
     },
   });
 
