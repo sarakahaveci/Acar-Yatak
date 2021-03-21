9cabab82320f9ba14a1b8a4a7868265a4b97474d
Yamoonz <yamooonz@gmail.com>
2021-03-21 14:11:46 +0300

    deleted unncessary files

--- src/i18n.js
+++ src/i18n.js
@@ -1,39 +1,27 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+// don't want to use this?
+// have a look at the Quick start guide
+// for passing in lng and translations on init
 
 i18n
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'en'
-  });
-
-
-const resources = {
-  en: {
-    translations: {
-      welcome: "Hello <br/> <strong>World</strong>"
-    }
-  },
-  ar: {
-    translation: {
-      "header": "اهلا وسهلا بكم في موقع اجار للمفروشات"
-    }
-  },
-  tur : {
-    translation: {
-      "header": "hoşgeldiniz"
-    }
-  },
-};
-
-i18n
+  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+  // learn more: https://github.com/i18next/i18next-http-backend
+  .use(Backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    resources,
-    lng: 'ar',
-    fallbackLng: 'tur',
+    fallbackLng: "en",
+    debug: true,
+
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
