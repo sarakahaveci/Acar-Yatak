13c17a7fed66e286491d7b8533f39a80b604c924
Yamoonz <yamooonz@gmail.com>
2021-03-25 14:43:59 +0300

    --- Auto-Git Commit ---

--- src/new.js
+++ src/new.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Conver extends Component {
-        
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  componentDidMount() {
+    googleTranslateElementInit(() => {
+      new google.translate.TranslateElement(
+        { pageLanguage: "en" },
+        "google_translate_element"
+      );
+    });
+    const script = document.createElement("script");
+    script.src =
+      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    script.async = true;
+    document.body.appendChild(script);
+  }
+  render() {
+    return (
+      <div>
+        <script
+          type="text/javascript"
+          src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+        />
+        <div id="google_translate_element"></div>
+      </div>
+    );
+  }
 }
