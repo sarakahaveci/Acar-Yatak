8be5784c4f432bb3c1a18bc18b6fdf75e0c87bf3
Yamoonz <yamooonz@gmail.com>
2021-03-28 21:31:56 +0300

    --- Auto-Git Commit ---

--- src/components/dropdown/TwoPersonBase.js
+++ src/components/dropdown/TwoPersonBase.js
@@ -9,9 +9,9 @@ function TwoPersonBase() {
         <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/z6NWvfb.jpg/100px180" />
   <Card.Body>
-    <Card.Title>APATİT Basement 01</Card.Title>
+    <Card.Title>APATİT . 01</Card.Title>
     <Card.Text>
-      Only Basement 
+      Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -22,9 +22,9 @@ function TwoPersonBase() {
 <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/lZpUP0N.jpg/100px180" />
   <Card.Body>
-    <Card.Title>APATİT Basement 02 </Card.Title>
+    <Card.Title>APATİT . 02 </Card.Title>
     <Card.Text>
-      Only Basement 
+      Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -35,9 +35,9 @@ function TwoPersonBase() {
 <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/atjYV88.jpg/100px180" />
   <Card.Body>
-    <Card.Title>EKO Basement 01 </Card.Title>
+    <Card.Title>EKO . 01 </Card.Title>
     <Card.Text>
-      Only Basement 
+      Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -48,9 +48,9 @@ function TwoPersonBase() {
 <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/0YvULSH.jpg/100px180" />
   <Card.Body>
-    <Card.Title>EKO Basement 02</Card.Title>
+    <Card.Title>EKO . 02</Card.Title>
     <Card.Text>
-      Only Basement 
+      Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -65,9 +65,9 @@ function TwoPersonBase() {
         <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/IrOaPPF.jpg/100px180" />
   <Card.Body>
-    <Card.Title>ELMAS Basement 01</Card.Title>
+    <Card.Title>ELMAS . 01</Card.Title>
     <Card.Text>
-      Only Basement 
+      Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -78,9 +78,9 @@ function TwoPersonBase() {
 <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/79iR7JA.jpg/100px180" />
   <Card.Body>
-    <Card.Title>ELMAS Basement 02 </Card.Title>
+    <Card.Title>ELMAS . 02 </Card.Title>
     <Card.Text>
-      Only Basement 
+      Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -91,9 +91,9 @@ function TwoPersonBase() {
 <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/NrcWGeI.jpg/100px180" />
   <Card.Body>
-    <Card.Title>GARNET Basement 01 </Card.Title>
+    <Card.Title>GARNET . 01 </Card.Title>
     <Card.Text>
-      Only Basement 
+     Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -104,9 +104,9 @@ function TwoPersonBase() {
 <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/MiYT6Ow.jpg/100px180" />
   <Card.Body>
-    <Card.Title>GARNET Basement 02</Card.Title>
+    <Card.Title>GARNET . 02</Card.Title>
     <Card.Text>
-      Only Basement 
+     Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -121,9 +121,9 @@ function TwoPersonBase() {
         <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/TOR0Niy.jpg/100px180" />
   <Card.Body>
-    <Card.Title>ONİKS Basement 01</Card.Title>
+    <Card.Title>ONİKS . 01</Card.Title>
     <Card.Text>
-      Only Basement 
+     Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -134,9 +134,9 @@ function TwoPersonBase() {
 <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/x2lm4Ci.jpg/100px180" />
   <Card.Body>
-    <Card.Title>ONİKS Basement 02 </Card.Title>
+    <Card.Title>ONİKS . 02 </Card.Title>
     <Card.Text>
-      Only Basement 
+     Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -147,9 +147,9 @@ function TwoPersonBase() {
 <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/7RI573W.jpeg/100px180" />
   <Card.Body>
-    <Card.Title>PIRALNTA Basement 01</Card.Title>
+    <Card.Title>PIRALNTA . 01</Card.Title>
     <Card.Text>
-      Only Basement 
+     Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -160,9 +160,9 @@ function TwoPersonBase() {
 <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/klDTXSf.jpeg/100px180" />
   <Card.Body>
-    <Card.Title>PIRALNTA Basement 02</Card.Title>
+    <Card.Title>PIRALNTA . 02</Card.Title>
     <Card.Text>
-      Only Basement 
+     Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -177,9 +177,9 @@ function TwoPersonBase() {
         <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/GAvvLYm.jpeg/100px180" />
   <Card.Body>
-    <Card.Title>SAFİR Basement 01</Card.Title>
+    <Card.Title>SAFİR . 01</Card.Title>
     <Card.Text>
-      Only Basement 
+     Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -190,9 +190,9 @@ function TwoPersonBase() {
 <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/wnz7eL4.jpeg/100px180" />
   <Card.Body>
-    <Card.Title>SAFİR Basement 02 </Card.Title>
+    <Card.Title>SAFİR . 02 </Card.Title>
     <Card.Text>
-      Only Basement 
+     Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -203,9 +203,9 @@ function TwoPersonBase() {
 <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/SX26Zlz.jpeg/100px180" />
   <Card.Body>
-    <Card.Title>ZÜMRÜT Basement 01 </Card.Title>
+    <Card.Title>ZÜMRÜT . 01 </Card.Title>
     <Card.Text>
-      Only Basement 
+     Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
@@ -216,9 +216,9 @@ function TwoPersonBase() {
 <Card style={{ width: '10rem' }}>
   <Card.Img variant="top" src="https://i.imgur.com/uT86ugO.jpeg/100px180" />
   <Card.Body>
-    <Card.Title>ZÜMRÜT Basement 02 </Card.Title>
+    <Card.Title>ZÜMRÜT . 02 </Card.Title>
     <Card.Text>
-      Only Basement 
+     Sadece Baza 
     </Card.Text>
     <div className="price">
    Price: 2,400₺ <br/>
