14a4aa11971788ddff826190489ba81807964f04
Yamoonz <yamooonz@gmail.com>
2021-03-31 19:16:49 +0300

    --- Auto-Git Commit ---

---
+++ src/SourceApi.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Admin, Resource, ListGuesser } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+
+const dataProvider =
+  jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+class SourceApi extends Component {
+  render() {
+    return (
+      <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser} />
+      </Admin>
+    );
+  }
+}
+export default SourceApi;
